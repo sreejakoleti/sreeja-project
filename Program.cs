5314b3eb52884364679c4b8e4a311a8533ac68dc
sreejakoleti <34535372+sreejakoleti@users.noreply.github.com>
2017-12-21 09:37:46 +0530

    program
    first sample code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Chapter1_BasicCodeInGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!Chapter1");
+            Console.ReadLine();
+        }
+    }
+}
